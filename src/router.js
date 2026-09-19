cd3e8aada2f15290fdf55d791a4b5ccedd65bd8b
slplaine <simoneleca@gmail.com>
2026-09-18 22:26:54 -0300

    Complete Swagger documentation

--- src/router.js
+++ src/router.js
@@ -56,16 +56,221 @@ router.get('/books', getBooksHandler);
  *         description: Unable to retrieve book
  */
 router.get('/books/:id', getBookByIdHandler);
-
+/**
+ * @openapi
+ * /books:
+ *   post:
+ *     summary: Create a book
+ *     tags:
+ *       - Books
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id
+ *               - authorId
+ *               - title
+ *               - publicationDate
+ *             properties:
+ *               id:
+ *                 type: string
+ *               authorId:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               publicationDate:
+ *                 type: string
+ *           example:
+ *             id: b4
+ *             authorId: a1
+ *             title: New Book
+ *             publicationDate: 2024-01-01
+ *     responses:
+ *       201:
+ *         description: Book created
+ *       400:
+ *         description: Invalid book data
+ */
 router.post('/books', createBookHandler);
+/**
+ * @openapi
+ * /books/{id}:
+ *   put:
+ *     summary: Update a book
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               authorId:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               publicationDate:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Book updated
+ *       404:
+ *         description: Book not found
+ */
 router.put('/books/:id', updateBookHandler);
+/**
+ * @openapi
+ * /books/{id}:
+ *   delete:
+ *     summary: Delete a book
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Book deleted
+ *       404:
+ *         description: Book not found
+ */
 router.delete('/books/:id', deleteBookHandler);
-
+/**
+ * @openapi
+ * /authors:
+ *   get:
+ *     summary: Get all authors
+ *     tags:
+ *       - Authors
+ *     responses:
+ *       200:
+ *         description: A list of authors
+ *       500:
+ *         description: Unable to retrieve authors
+ */
 router.get('/authors', getAuthorsHandler);
+/**
+ * @openapi
+ * /authors/{id}:
+ *   get:
+ *     summary: Get author by ID
+ *     tags:
+ *       - Authors
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Author returned successfully
+ *       404:
+ *         description: Author not found
+ *       500:
+ *         description: Unable to retrieve author
+ */
 router.get('/authors/:id', getAuthorByIdHandler);
-
+/**
+ * @openapi
+ * /authors:
+ *   post:
+ *     summary: Create an author
+ *     tags:
+ *       - Authors
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id
+ *               - name
+ *               - birthYear
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               birthYear:
+ *                 type: number
+ *           example:
+ *             id: a4
+ *             name: Example Author
+ *             birthYear: 1980
+ *     responses:
+ *       201:
+ *         description: Author created
+ *       400:
+ *         description: Invalid author data
+ */
 router.post('/authors', createAuthorHandler);
+/**
+ * @openapi
+ * /authors/{id}:
+ *   put:
+ *     summary: Update an author
+ *     tags:
+ *       - Authors
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               birthYear:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Author updated
+ *       404:
+ *         description: Author not found
+ */
 router.put('/authors/:id', updateAuthorHandler);
+/**
+ * @openapi
+ * /authors/{id}:
+ *   delete:
+ *     summary: Delete an author
+ *     tags:
+ *       - Authors
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Author deleted
+ *       404:
+ *         description: Author not found
+ *       409:
+ *         description: Author still has books
+ */
 router.delete('/authors/:id', deleteAuthorHandler);
 
 export default router;
