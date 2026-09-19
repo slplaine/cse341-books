b04e6dd6a0c13f1122c293a69c76de7f9b2d0cde
slplaine <simoneleca@gmail.com>
2026-09-18 21:20:31 -0300

    Implement author collection API

--- src/router.js
+++ src/router.js
@@ -1,9 +1,28 @@
 import express from 'express';
-import { getBooksHandler,getBookByIdHandler } from './controllers/books.js';
+
+import {
+  getBooksHandler,
+  getBookByIdHandler,
+} from './controllers/books.js';
+
+import {
+  getAuthorsHandler,
+  getAuthorByIdHandler,
+  createAuthorHandler,
+  updateAuthorHandler,
+  deleteAuthorHandler,
+} from './controllers/authors.js';
 
 const router = express.Router();
 
 router.get('/books', getBooksHandler);
 router.get('/books/:id', getBookByIdHandler);
+
+router.get('/authors', getAuthorsHandler);
+router.get('/authors/:id', getAuthorByIdHandler);
+
+router.post('/authors', createAuthorHandler);
+router.put('/authors/:id', updateAuthorHandler);
+router.delete('/authors/:id', deleteAuthorHandler);
 
 export default router;
